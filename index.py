d0be56b4d0df4d83204fb8e7a08f0c95c62bc301
giordano200699 <giordano200699@gmail.com>
2019-08-08 16:43:44 -0500

    Crear y Visualizar stands

--- index.py
+++ index.py
@@ -1,8 +1,17 @@
-from flask import Flask, render_template, request,session 
+from flask import Flask, render_template, request,session
+from pymongo import MongoClient
 
 aplicacion = Flask(__name__)
 
 aplicacion.secret_key = 'esto-es-mi-clave-secreta'
+
+
+
+client = MongoClient("mongodb+srv://giordano:waldo@cluster0-p2txm.mongodb.net/Almacen?retryWrites=true")
+mydb = client["Almacen"]
+
+stands = mydb["stands"]
+
 
 @aplicacion.route('/')
 def index():
@@ -24,7 +33,8 @@ def panel():
 def panel2():
 	if(session['nombreAdministrador']!='Mirian' or session['clave']!='mayra'):
 		return 'Usted no ha iniciado sesión'
-	return render_template('panel/panel.html')
+	standsC = stands.find()
+	return render_template('panel/panel.html',standsC=standsC)
 
 @aplicacion.route('/logout',methods = ['POST'])
 def logout():
@@ -35,6 +45,30 @@ def logout():
 		print("Boto error")
 	return render_template('index/index.html')
 
+@aplicacion.route('/crearStand',methods = ['GET'])
+def crearStand():
+	if(session['nombreAdministrador']!='Mirian' or session['clave']!='mayra'):
+		return 'Usted no ha iniciado sesión'
+	return render_template('panel/crearStand.html')
+
+@aplicacion.route('/crearStand',methods = ['POST'])
+def crearStandP():
+	if(session['nombreAdministrador']!='Mirian' or session['clave']!='mayra'):
+		return 'Usted no ha iniciado sesión'
+	if(request.form.get('nombreStand')==''):
+		return 'Los datos ingresados son inválidos'
+	standArrU = []
+	standU = stands.find().sort('standId', -1).limit(1)
+	for standEU in standU:
+		standArrU.append(standEU)
+	if(len(standArrU)>0):
+		standId = standArrU[0]['standId'] + 1
+	else:
+		standId = 1
+	
+	x = stands.insert_one({ "nombreStand": request.form.get('nombreStand'), "standId":standId })
+	standsC = stands.find()
+	return render_template('panel/panel.html',standsC=standsC)
 
 if __name__ == '__main__':
 	aplicacion.run(debug=True)
